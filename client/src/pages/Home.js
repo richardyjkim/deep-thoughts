f9352c4b35df69dda4cf3ca57f5741ce56a643b0
Richard <richardyjkim@gmail.com>
2021-06-21 16:50:50 -0700

    thoughtform constructed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHTS } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -18,6 +18,11 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
@@ -34,7 +39,7 @@ const Home = () => {
           </div>
         ) : null}
       </div>
-    </main>
+    </main >
   );
 };
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -80,3 +80,4 @@ export const REMOVE_FRIEND = gql`
     }
   }
 `;
+
